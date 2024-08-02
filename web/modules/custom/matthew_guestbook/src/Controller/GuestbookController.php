b5c2682b1c00914a51afa02f07d75864743af66e
mvovchuk <m.vovchuk@edu.internetdevels.com>
2024-08-02 14:47:12 +0300

    task/342778: Added a page with all user reviews

--- web/modules/custom/matthew_guestbook/src/Controller/GuestbookController.php
+++ web/modules/custom/matthew_guestbook/src/Controller/GuestbookController.php
@@ -4,10 +4,11 @@ namespace Drupal\matthew_guestbook\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Database;
+use Drupal\file\Entity\File;
 
 class GuestbookController extends ControllerBase {
 
-  public function content() {
+  public function content2() {
     $query = Database::getConnection()->select('guestbook_entries', 'g')
       ->fields('g', ['id', 'name', 'email', 'phone', 'message', 'created'])
       ->orderBy('created', 'DESC');
@@ -16,6 +17,53 @@ class GuestbookController extends ControllerBase {
     return [
       '#theme' => 'guestbook-entries',
       '#entries' => $results,
+      '#attached' => [
+        'library' => [
+          'matthew_guestbook/styles',
+        ],
+      ],
+    ];
+  }
+  public function content() {
+    $query = Database::getConnection()->select('guestbook_entries', 'g')
+      ->fields('g', [
+        'id',
+        'name',
+        'email',
+        'phone',
+        'message',
+        'review',
+        'avatar_fid',
+        'review_image_fid',
+        'created',
+      ])
+      ->orderBy('created', 'DESC');
+    $results = $query->execute()->fetchAll();
+
+    // Process the results to get file URLs.
+    foreach ($results as &$result) {
+      if ($result->avatar_fid) {
+        $file = File::load($result->avatar_fid);
+        if ($file) {
+          $result->avatar_url = $file->createFileUrl();
+        }
+      }
+      if ($result->review_image_fid) {
+        $file = File::load($result->review_image_fid);
+        if ($file) {
+          $result->review_image_url = $file->createFileUrl();
+        }
+      }
+    }
+
+    return [
+      '#theme' => 'guestbook-entries',
+      '#entries' => $results,
+      '#attached' => [
+        'library' => [
+          'matthew_guestbook/styles',
+        ],
+      ],
     ];
   }
 
